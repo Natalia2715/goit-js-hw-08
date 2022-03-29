9a425e575b658b725bb85dc52b5085d0e9938f01
Natalia2715 <martinuknatala77@gmail.com>
2022-03-29 09:14:41 +0300

    npm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,7 @@
- const iframe = document.querySelector('iframe');
+    const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
+    const LOCALSTORAGE_KEY = "videoplayer-current-time";
+    import throttle from 'lodash.throttle';
 
     player.on('play', function() {
         console.log('played the video!');
@@ -8,3 +10,9 @@
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
+const onPlay = function (data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+    }
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    emailInput: document.querySelector("input"),
+    messageInput: document.querySelector("textarea"),
+};
+
+const formData = {
+    email: "",
+    message: "",
+};
+
+const KEY = "feedback-form-state"
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateFeedback();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const fdb = localStorage.getItem(KEY);
+    console.log(fdb);
+    localStorage.clear();
+    event.currentTarget.reset();
+ };
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(KEY, JSON.stringify(formData));
+    };
+
+function populateFeedback() {
+    
+    const savedFbck = JSON.parse(localStorage.getItem(KEY));
+    const savedEmail = savedFbck.email;
+    const savedMessage = savedFbck.message;
+
+    if (savedFbck) {
+        localStorage.setItem(KEY, JSON.stringify(savedFbck));
+            refs.emailInput.value = savedEmail;
+            refs.messageInput.value = savedMessage;
+        formData.email = savedEmail;
+        formData.message = savedMessage;
+    }
+       
+}
